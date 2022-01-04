246ad30c27d91c201ee3fc0f9c327ccf9b14e3a4
Mo_Fares <94139202+MoFares7@users.noreply.github.com>
2022-01-04 12:49:39 +0200

    Add files via upload

---
+++ mainroom.h
@@ -0,0 +1,21 @@
+#pragma once
+class mainroom
+{
+public:
+	mainroom();
+
+	void mainroom::drawModel();
+
+private:
+	void table();
+	void table1();
+	void table2();
+	void table3();
+	void cases();
+	void cases1();
+	void cases2();
+	void cases3();
+	void fe();
+
+};
+
